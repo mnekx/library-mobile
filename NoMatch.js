9a77d07dbe3ddc9c5c558aa86d264278f0091c15
mnekx <karimnembac@gmail.com>
2022-10-01 14:41:59 +0300

    Update NoMatch.js

--- NoMatch.js
+++ NoMatch.js
@@ -1,7 +1,9 @@
-import { View } from "react-native-web";
+import { View, Text } from "react-native-web";
+import AppHeaderComp from './AppHeaderComp'
 
 const NoMatch = () => {
     return (<View>
+        <AppHeaderComp/>
         <Text style={{color: 'red'}}>Wrong route provided!</Text>
     </View>)
 }
