d03a5d6bae5e236665fea62624d6cbf8ff1da627
mnekx <karimnembac@gmail.com>
2022-10-01 14:41:36 +0300

    Update AddBookComp.js

--- features/books/AddBookComp.js
+++ features/books/AddBookComp.js
@@ -1,27 +1,58 @@
-import { ScrollView, TextInput, Text } from 'react-native-web';
-import { useState } from "react";
+import { useState } from 'react';
+import { Button, ScrollView, TextInput, Text } from 'react-native-web';
+import { useAuthState } from '../../contexts/auth/auth-context';
+import { addBook } from '../../contexts/books/books-actions';
+import {
+  useBooksDispatchContext,
+} from '../../contexts/books/books-context';
+import { useNavigate } from 'react-router-native';
 
 const AddBookComp = () => {
   const [title, setTitle] = useState('');
   const [author, setAuthor] = useState('');
+  const [error, setError] = useState('');
+  const booksDispatchContext = useBooksDispatchContext();
+  const { token, userDetails } = useAuthState();
+  const navigate = useNavigate();
+ 
+  const handleSave = async () => {
+    const addedBook = await addBook(
+      booksDispatchContext,
+      {title, author},
+      token,
+      userDetails
+    );
+    console.log(addedBook)
+    if (typeof addedBook?._id === 'string' && addedBook?._id?.length > 0) {
+      setError('');
+      navigate('/');
+    }
+    if (addedBook?.error) setError(addedBook?.error);
+  };
   return (
-    <ScrollView style={{marginTop: '4rem'}}>
-      <Text>Add New Book</Text>
-      <TextInput
-        style={{ height: 40 }}
-        placeholder='Enter Title'
-        onChangeText={(newText) => setTitle(newText)}
-        defaultValue={title}
-        name='title'
-      />
-      <TextInput
-        style={{ height: 40 }}
-        placeholder='Enter Author'
-        onChangeText={(newText) => setAuthor(newText)}
-        defaultValue={author}
-        name='author'
-      />
-    </ScrollView>
+    <>
+      <Text style={{marginTop: '2rem'}}>Add Book</Text>
+
+      <ScrollView>
+        <TextInput
+          style={{ height: 40 }}
+          placeholder='Enter Title'
+          onChangeText={(newText) => setTitle(newText)}
+          defaultValue={title}
+          name='title'
+        />
+        <TextInput
+          style={{ height: 40 }}
+          placeholder='Enter Author'
+          onChangeText={(newText) => setAuthor(newText)}
+          defaultValue={author}
+          name='author'
+        />
+        <Button onPress={handleSave} title='Save' />
+        
+      </ScrollView>
+      {error?.length > 0 ? <Text style={{ color: 'red' }}>{error}</Text> : ''}
+    </>
   );
 };
 
