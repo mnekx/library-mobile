711c598f94225c167bba25cfe91054b8bfbe4536
mnekx <karimnembac@gmail.com>
2022-10-01 06:11:22 +0300

    Create NoMatch.js

---
+++ NoMatch.js
@@ -0,0 +1,9 @@
+import { View } from "react-native-web";
+
+const NoMatch = () => {
+    return (<View>
+        <Text style={{color: 'red'}}>Wrong route provided!</Text>
+    </View>)
+}
+
+export default NoMatch
