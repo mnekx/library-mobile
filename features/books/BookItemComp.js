9e3fd7207872ffd68675da54c4ae725c67bbe78f
mnekx <karimnembac@gmail.com>
2022-10-01 14:41:39 +0300

    Update BookItemComp.js

--- features/books/BookItemComp.js
+++ features/books/BookItemComp.js
@@ -1,16 +1,34 @@
-import { StyleSheet, View, Button } from 'react-native-web';
-import {useNavigate} from 'react-router-native'
+import { useState } from "react";
+import { StyleSheet, View, Button, Text } from 'react-native-web';
+import {redirect, useNavigate} from 'react-router-native'
+import { useAuthState } from "../../contexts/auth/auth-context";
+import { deleteBook } from "../../contexts/books/books-actions";
+import { useBooksDispatchContext } from "../../contexts/books/books-context";
 
 const BookItemComp = ({ book }) => {
   const navigate = useNavigate()
+  const dispatch = useBooksDispatchContext()
+  const {token, userDetails} = useAuthState()
+  const [error, setError] = useState('')
   const handleEdition = () => {
     navigate('/edit-book/'+book._id)
   }
-  const handleDeletion = () => {}
+  const handleDeletion =async () => {
+    const deletionResponse = await deleteBook(dispatch, book._id, token, userDetails)
+    
+    if (deletionResponse?.acknowledged) {
+      setError('')
+      navigate(-1)
+      navigate('/')
+    }
+    if(deletionResponse?.error?.length > 0) setError(deletionResponse.error.name)
+    else setError('!')
+  }
   return (
     <View style={styles.item}>
       <Button style={styles.editButton} title={book.title} onPress={handleEdition}/>
-      <Button style={styles.deleteButton} title='Delete' onPress={handleDeletion} />
+      <Button style={styles.deleteButton} color='red' title='Delete' onPress={handleDeletion} />
+      {error.length > 0? <Text style={{color: 'red'}}>{error}</Text>: <></>}
     </View>
   );
 };
@@ -24,7 +42,7 @@ const styles = StyleSheet.create({
     justifyContent: 'space-between'
   },
   deleteButton: {
-    color: 'red',
+    backgroundColor: 'red',
     flexBasis: '5%',
   },
   editButton: {
