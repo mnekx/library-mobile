a8f67068b63a9f320fc9757003f6340d59d9fa1f
mnekx <karimnembac@gmail.com>
2022-10-01 06:09:40 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { View } from 'react-native';
+import HomeComp from './HomeComp';
+import RegisterComp from './RegisterComp';
+import LoginComp from './LoginComp';
+import { NativeRouter, Routes, Route } from 'react-router-native';
+import BooksComp from './features/books/BooksComp';
+import AddBookComp from './features/books/AddBookComp';
+import EditBookComp from './features/books/EditBookComp';
+import NoMatch from './NoMatch';
+import { AuthProvider } from './contexts/auth/aut-context';
+import { ProtectedRoute } from './features/protected-route';
+import { BooksProvider } from './contexts/books/books-context'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NativeRouter>
+      <AuthProvider>
+        <BooksProvider>
+        <View>
+          <Routes>
+            <Route
+              path='/'
+              element={
+                <ProtectedRoute>
+                  <HomeComp />
+                </ProtectedRoute>
+              }
+            >
+              <Route
+                index
+                element={
+                  <ProtectedRoute>
+                    <BooksComp />
+                  </ProtectedRoute>
+                }
+              ></Route>
+              <Route
+                path='add-book'
+                element={
+                  <ProtectedRoute>
+                    <AddBookComp />
+                  </ProtectedRoute>
+                }
+              ></Route>
+              <Route
+                path='edit-book/:id'
+                element={
+                  <ProtectedRoute>
+                    <EditBookComp />
+                  </ProtectedRoute>
+                }
+              ></Route>
+            </Route>
+            <Route path='/register' element={<RegisterComp />}></Route>
+            <Route path='/login' element={<LoginComp />}></Route>
+            <Route path='*' element={<NoMatch />} />
+          </Routes>
+        </View></BooksProvider>
+      </AuthProvider>
+    </NativeRouter>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
